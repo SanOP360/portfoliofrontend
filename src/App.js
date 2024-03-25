67c40ddd82db10c43050442ece4eec09b76c5dd7
SanOP360 <sanjayl2001alm@gmail.com>
2024-03-25 23:15:43 +0530

    New App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import Header from "./components/Header";
 import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
-import AddProjectForm from "./components/AddProjectForm";
 import About from "./components/About";
 import { Element } from "react-scroll";
 import Contact from "./components/ContactUs";
@@ -42,11 +41,6 @@ const App = () => {
         <Element name="contact-section">
           <Contact/>
         </Element>
-        <Routes>
-          <Route path="/project" element={<Portfolio projects={projects} />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/add-project" element={<AddProjectForm />} />
-        </Routes>
         <Footer />
       </Router>
     </>
